21d142024f798210b7a480a9f1183a4067beb68b
Yoav Lavi <yoavlavi122@gmail.com>
2022-02-19 17:09:44 +0100

    fix qualifier

--- crates/melody_wasm/src/lib.rs
+++ crates/melody_wasm/src/lib.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compiler(source: &str) -> Result<String, JsValue> {
-    Ok(melody::compiler(&source).expect_throw("Encountered a parsing error"))
+    Ok(melody_compiler::compiler(&source).expect_throw("Encountered a parsing error"))
 }
