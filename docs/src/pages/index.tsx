18e23536e7c4eca2df7f1757085cd7aee670e239
Yoav Lavi <yoavlavi122@gmail.com>
2022-02-21 21:09:55 +0100

    auto redirect to docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,25 +1,6 @@
 import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
+import { Redirect } from "react-router-dom";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  return (
-    <Layout title={`Melody Documentation`} description="Melody Documentation">
-      <HomepageHeader />
-    </Layout>
-  );
+export default function Home() {
+  return <Redirect to="/melody/docs/install" />;
 }
