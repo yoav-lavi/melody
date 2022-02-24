158ade0064fd3e7977c64bfd70087f5560105b8b
Yoav Lavi <yoavlavi122@gmail.com>
2022-02-24 18:23:17 +0100

    formatting

--- crates/melody_wasm/src/lib.rs
+++ crates/melody_wasm/src/lib.rs
@@ -7,7 +7,7 @@ pub fn compiler(source: &str) -> Result<String, JsValue> {
     let raw_output = melody_compiler::compiler(source);
     if let Err(error) = raw_output {
         return Err(JsValue::from(format!(
-            "Error: Unexpected {} on line {}",
+            "Error: Unexpected \"{}\" on line {}",
             error.token,
             error.line_index + 1
         )));
