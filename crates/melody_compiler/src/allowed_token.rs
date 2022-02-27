8806f91ce6d04f211498798dcdcf5b97ea9f390b
Yoav Lavi <yoavlavi122@gmail.com>
2022-02-27 19:23:52 +0100

    cleanup

--- crates/melody_compiler/src/allowed_token.rs
+++ crates/melody_compiler/src/allowed_token.rs
@@ -1,39 +1,40 @@
 use crate::lexer::tokens::Token;
 use crate::lexer::tokens::TokenType;
 
+macro_rules! match_or_err {
+    ($condition: expr, $err: literal) => {
+        if $condition {
+            Ok(())
+        } else {
+            Err(String::from($err))
+        }
+    };
+}
+
 pub fn allow_next(token: &Token) -> Result<(), String> {
-    if [TokenType::Newline, TokenType::Semicolon].contains(&token.to_type()) {
-        Ok(())
-    } else {
-        Err(String::from("expected a semicolon or newline"))
-    }
+    match_or_err!(
+        [TokenType::Newline, TokenType::Semicolon].contains(&token.to_type()),
+        "expected a semicolon or newline"
+    )
 }
 
 pub fn allow_newline(token: &Token) -> Result<(), String> {
-    if token.to_type() == TokenType::Newline {
-        Ok(())
-    } else {
-        Err(String::from("expected a newline"))
-    }
+    match_or_err!(token.to_type() == TokenType::Newline, "expected a newline")
 }
 
 pub fn allow_atom(token: &Token) -> Result<(), String> {
-    if [
-        TokenType::Literal,
-        TokenType::Group,
-        TokenType::Symbol,
-        TokenType::Range,
-        TokenType::Raw,
-        TokenType::Assertion,
-    ]
-    .contains(&token.to_type())
-    {
-        Ok(())
-    } else {
-        Err(String::from(
-            "expected a literal, raw, group, symbol, or range",
-        ))
-    }
+    match_or_err!(
+        [
+            TokenType::Literal,
+            TokenType::Group,
+            TokenType::Symbol,
+            TokenType::Range,
+            TokenType::Raw,
+            TokenType::Assertion,
+        ]
+        .contains(&token.to_type()),
+        "expected a literal, raw, group, symbol, or range"
+    )
 }
 
 pub fn allowed_token(previous: &Option<TokenType>, current: &Token) -> Result<(), String> {
