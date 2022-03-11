21cd4d9b5e61d3829e3fe3cc233815af5026f20c
Yoav Lavi <yoavlavi122@gmail.com>
2022-03-11 22:14:15 +0100

    auto focus editor

--- playground/src/editors/index.ts
+++ playground/src/editors/index.ts
@@ -35,16 +35,23 @@ const getEditorInitialValue = () => {
   return DEFAULT_EDITOR_VALUE;
 };
 
+const initialValue = getEditorInitialValue();
+
 export const initEditors = async () => {
   if (!melodyEditorTarget || !regexEditorTarget) {
     return;
   }
 
   const melodyEditor = editor.create(melodyEditorTarget, {
-    value: getEditorInitialValue(),
+    value: initialValue,
     language: MELODY_LANGUAGE_ID,
     ...DEFAULT_EDITOR_SETTINGS,
   });
+
+  const lineNumber = initialValue.split('\n').length + 1;
+
+  melodyEditor.setPosition({ lineNumber, column: 0 });
+  melodyEditor.focus();
 
   const regexEditor = editor.create(regexEditorTarget, {
     value: '',
