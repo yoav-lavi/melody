b8377ca44b19091d2192e6ce328f00fa877c762b
Yoav Lavi <yoavlavi122@gmail.com>
2022-06-25 15:27:44 +0200

    visual improvements

--- playground/src/copy-button/index.ts
+++ playground/src/copy-button/index.ts
@@ -4,6 +4,9 @@ enum CopyButtonTextState {
   Default = 'Copy Link to Source',
 }
 
+const COPY_BUTTON_HIGHLIGHT = '#3B4252CC';
+const COPY_BUTTON_COLOR = '#2E3440';
+
 const copyButton = document.getElementById('copy-button');
 const originalText = copyButton?.textContent ?? CopyButtonTextState.Default;
 
@@ -15,11 +18,13 @@ const onCopyButtonClick = async (copyButton: HTMLElement) => {
       )}`,
     );
     copyButton.textContent = CopyButtonTextState.Copied;
+    copyButton.style.background = COPY_BUTTON_HIGHLIGHT;
   } catch (error) {
     copyButton.textContent = CopyButtonTextState.Error;
   } finally {
     setTimeout(() => {
       copyButton.textContent = originalText;
+      copyButton.style.background = COPY_BUTTON_COLOR;
     }, 3000);
   }
 };
